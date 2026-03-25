bf2948c6c9d200618b053e9ba68dc19fd03413a2
masonJS <mwoodev526@gmail.com>
2026-03-25 18:33:51 +0900

    feat: add barrel export

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+// Types
+export * from './opensearch/opensearch.type.js';
+export * from './opensearch/opensearch.response.type.js';
+
+// Query Builder
+export * from './opensearch/opensearch.query-builder.js';
+
+// Index Mapping Builder
+export * from './opensearch/opensearch.index-mapping-builder.js';
+
+// NestJS Module & Services
+export * from './opensearch/opensearch.module.js';
+export * from './opensearch/opensearch.document-service.js';
+export * from './opensearch/opensearch.search-service.js';
+export * from './opensearch/opensearch.index-service.js';
